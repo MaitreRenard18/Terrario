1dfe0ebff1ac24be8abf5691aeb2355dd59714ac
Wagister <61418245+Wagister@users.noreply.github.com>
2022-05-16 22:50:37 +0200

    Update Terrario.py

--- Terrario.py
+++ Terrario.py
@@ -30,7 +30,7 @@ layers = {
     3: ["dirt", "dirt", "stone"],
     4: ["dirt", "stone"],
     5: ["stone"],
-    8: ["stone" for _ in range(16)] + ["coal"], #Ici par exemple le charbon à une chance sur 17 de se générer de la coucge 8 à 11
+    8: ["stone" for _ in range(16)] + ["coal"], # Ici par exemple le charbon à une chance sur 17 de se générer de la coucge 8 à 11
     12: ["stone" for _ in range(32)] + ["coal", "coal", "iron"],
     64: ["stone" for _ in range(64)] + ["iron", "iron", "gold"],
     256: ["stone" for _ in range(128)] + ["gold", "gold", "diamond"],
@@ -136,21 +136,22 @@ ores_values = {
     "gold": 4,
     "diamond": 8,
     "ruby": 15
-}
+} #Dictionnaire contenant l'or donné pour chaque minerai
 
 class Player:
     def __init__(self, position, map):
-        self.position = position
-        self.direction = ["right", (1, 0)]
+        self.position = position #Prend en paramètre la position du joueur 
+        self.direction = ["right", (1, 0)] #Défini la direction vers laquelle le drill va
 
-        self.speed = 1
+        self.speed = 1 #Le montant qui défini la vitesse du joueur, maximum 10, sinon le drill se téléporte
         self.moving_cooldown = 0
         self.falling_cooldown = 0
 
-        self.gold = 0
-        self.fuel = 100
+        self.gold = 0 #Le montant qui défini l'argent
+        self.fuel_max = 50 #Le montant qui défini le fuel max, modifiable par le shop
+        self.fuel = self.fuel_max #ça c'est pour définir le fuel durant le jeu
         
-        self.map = map
+        self.map = map #Prend en paramètre la class map, pour avoir accès à ses informations
 
     def get_camera_offset(self):
         screensize = screen.get_size()
@@ -161,12 +162,12 @@ class Player:
 
     def mine(self):
         x, y = self.position
-        ore = self.map.tiles[x][y]
+        ore = self.map.tiles[x][y] #Variable qui regarde le bloc où que le joueur est en train de miner
 
-        if ore in ores_values:
-            self.gold += ores_values[ore]
+        if ore in ores_values: #Si le joueur mine un bloc qui se trouve dans le dictionnaire des minerais
+            self.gold += ores_values[ore] #Le gold augmente selon la valeur attribuée au minerai cassé
 
-        self.map.tiles[x][y] = "cave"
+        self.map.tiles[x][y] = "cave" #De plus, on affiche une texture de cave pour préciser que le bloc est miné
 
     def tick(self):
         screensize = screen.get_size()
@@ -181,18 +182,18 @@ class Player:
 
         x, y = self.position
 
-        if not self.map.tiles[x][y] in ["scaffolding", "cave", "air"]:
-            self.mine()
+        if not self.map.tiles[x][y] in ["scaffolding", "cave", "air"]: #si le bloc sur lequel on veut aller n'est pas un échaffaudage/une cave/de l'air
+            self.mine() #Le joueur peut le miner
         
-        if self.map.tiles[x][y + 1] == "cave":
+        if self.map.tiles[x][y + 1] == "cave": #Si le bloc en dessous du joueur est une cave
             parachue_texture = pygame.transform.scale(textures["parachute"], (32, 32))
-            screen.blit(parachue_texture, (screensize[0] // 2 - 16, screensize[1] // 2 - 32))
+            screen.blit(parachue_texture, (screensize[0] // 2 - 16, screensize[1] // 2 - 32)) #On affiche l'image de parachute au dessus du drill
 
             if self.falling_cooldown > 0:
                 self.falling_cooldown -= .25
                 return
             
-            self.position = (x, self.position[1] + 1)
+            self.position = (x, self.position[1] + 1) #Le drill descend d'un bloc, pour simuler de la gravité
             self.falling_cooldown = 1
 
             return
@@ -201,37 +202,45 @@ class Player:
             self.moving_cooldown -= self.speed * .1
             return
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT] and self.map.tiles[x + 1][y] != "bedrock":
-            self.position = (x + 1, y)
-            self.direction = ["right", (1, 0)]
-            self.fuel -= 1
+        if self.fuel < 0: #Si le drill ne contient plus de fuel
+            self.position = (self.map.width // 2, 0) #Il revient à sa position originale
+            screen.blit(textures["nofuel"], (348,348)) #On affiche la fenetre indiquant qu'il n'y a plus de fuel
+            pygame.display.flip()
+            sleep(2) #On attend 2 secondes que le joueur perçoit bien la fenetre
+            self.fuel = self.fuel_max #On réeinitialise le fuel
+            return
+
+        keys = pygame.key.get_pressed() #On stock le touche pressée dans la variable keys
+        if keys[pygame.K_RIGHT] and self.map.tiles[x + 1][y] != "bedrock": #Si la touche préssée est droite, et qu'il n'y a pas de bedrock à droite
+            self.position = (x + 1, y) #La position du joueur augmente, vers la droite
+            self.direction = ["right", (1, 0)] #La direction est mise vers la droite
+            self.fuel -= 1 #Et on décrémente le fuel car le joueur vient de bouger
 
             self.moving_cooldown = 1
             return
 
-        if keys[pygame.K_LEFT] and self.map.tiles[x - 1][y] != "bedrock":
-            self.position = (x - 1, y)
-            self.direction = ["left", (-1, 0)]
-            self.fuel -= 1
+        if keys[pygame.K_LEFT] and self.map.tiles[x - 1][y] != "bedrock": #Si la touche préssée est gauche, et qu'il n'y a pas de bedrock à gauche
+            self.position = (x - 1, y) #La position du joueur diminue, pour aller à gauche
+            self.direction = ["left", (-1, 0)] #La direction est mise vers la gauche
+            self.fuel -= 1 #Et on décrémente le fuel car le joueur vient de bouger
 
             self.moving_cooldown = 1
             return
 
-        if keys[pygame.K_UP] and self.map.tiles[x][y - 1] != "bedrock" and y > 0:
-            self.position = (x, y - 1)
-            self.direction = ["up", (0, -1)]
-            self.fuel -= 1
+        if keys[pygame.K_UP] and self.map.tiles[x][y - 1] != "bedrock" and y > 0: #Si la touche préssée est haut, et qu'il n'y a pas de bedrock en haut
+            self.position = (x, y - 1) #La position du joueur diminue, pour aller en haut
+            self.direction = ["up", (0, -1)] #La direction est mise vers le haut
+            self.fuel -= 1 #Et on décrémente le fuel car le joueur vient de bouger
 
-            self.map.tiles[x][y] = "scaffolding"
+            self.map.tiles[x][y] = "scaffolding" #En plus, on affiche la texture d'échafaudage, pour éviter que le joueur soit soumis à la gravité
             
             self.moving_cooldown = 1
             return
 
-        if keys[pygame.K_DOWN] and self.map.tiles[x][y + 1] != "bedrock":
-            self.position = (x, y + 1)
-            self.direction = ["down", (0, 1)]
-            self.fuel -= 1
+        if keys[pygame.K_DOWN] and self.map.tiles[x][y + 1] != "bedrock": #Si la touche préssée est bas, et qu'il n'y a pas de bedrock en dessous
+            self.position = (x, y + 1) #La position du joueur augmente, pour aller en bas
+            self.direction = ["down", (0, 1)] #La direction est mise vers le bas
+            self.fuel -= 1 #Et on décrémente le fuel car le joueur vient de bouger
 
             self.moving_cooldown = 1
             return
@@ -241,18 +250,58 @@ class Interface:
     def __init__(self, player): #Prend en paramètre la class Player, pour avoir accès à ses variables
         self.player = player
         
-    def render(self):
-        screen.blit(textures["fuelrod"], (20, 20))
-        self.fuel = int(self.player.fuel*50/100)
-        for i in range(0,self.fuel*8,1):
+    def render(self): #La fonction qui affiche toute les textures de barres/compteurs
+        screen.blit(textures["fuelrod"], (20, 20)) #J'affiche la barre de fuel
+        fuel_amount = int(self.player.fuel/self.player.fuel_max*100) #Je converti le fuel en pourcentage, pour adapter ma barre selon le fuel max
+        for i in range(0,fuel_amount*2): #J'affiche la quantité de fuel selon le pourcentage
             screen.blit(textures["fuelp"], (86+i, 32))
 
-        screen.blit(textures["goldbackground"], (855, 23))
-        screen.blit(textures["coin"], (970, 15))
+        screen.blit(textures["goldbackground"], (855, 23)) #J'affiche le fond pour l'affichage du compteur de gold
+        screen.blit(textures["coin"], (970, 15)) #J'affiche l'icone de pièce, pour spécifier que c'est l'argent
         gold = [int(v) for v in list(str(self.player.gold))]
         for w in range(len(gold)):
             screen.blit(textures[str(gold[w])], (860+28*w, 27))
             pygame.display.flip()
+
+#Shop
+def affichage(): #C'est ma fonction qui affiche le shop de base, sans l'animation des boutons ou autre
+        screen.blit(textures["background"], (0,0)) #J'affiche le fond beige
+        screen.blit(textures["shop"], (70,50)) #J'affiche l'interface
+        pygame.display.flip()
+
+class Button:
+    def __init__(self, image, position, price, func, player): #Qui prend en paramètre une image, sa position, le prix de l'amélioration, une fonction pour augmenter/décrémenter selon le bouton, et la class Player (pour avoir accès au fuel et au gold)
+        self.image = image
+        self.position = position
+        self.price = price
+        self.func = func
+        self.player = player
+
+    def click(self):
+        for event in pygame.event.get(): 
+            if event.type == pygame.MOUSEBUTTONUP: #Si le joueur clique sur le bouton
+                self.func(self) #On effectue la fonction qui est définie sur le bouton cliqué
+                    
+    def animation(self): #ça sert à afficher les boutons animés, par dessus l'interface
+        mouse = pygame.mouse.get_pos() #On stock la position de la souris dans la variable mouse
+        while self.position[0]<mouse[0]<self.position[0] + self.image.get_width() and self.position[1]<mouse[1]<self.position[1] + self.image.get_height(): #Si la position de la souris se trouve entre la position du bouton, et le coin du bouton (opposé à la position de base du coup, pour faire une zone rectangulaire)
+            screen.blit(self.image, (self.position[0], self.position[1])) #On affiche l'animation du bouton correspondant
+            pygame.display.flip()
+            mouse = pygame.mouse.get_pos() #Je reprend la position de la souris, pour vérifier si elle est toujours sur le bouton ou non (donc pour sortir du while)
+            self.click() #J'appelle click, pour vérifier si le bouton est cliqué ou non
+        affichage() #En sortant du while (donc si la souris n'est plus sur le bouton), on réenitialise l'affichage du shop (pour enlever les potentielles animations affichées)
+    
+def speed_button(self): #Fonction qui augmente la vitesse du joueur et le prix de l'amélioration qui vient d'être achetée
+    self.player.speed += 0.2
+    self.price += 20
+def fuel_button(self): #Fonction qui augmente le fuel maximum et le prix de l'amélioration qui vient d'être achetée
+    self.player.fuel_max += 10
+    self.price += 20
+def skin_button(self): #Fonction qui change le skin du joueur
+    print("owo")
+def exit_button(self): #Fonction définie pour le bouton exit, pour sortir du shop, et retourner sur la map
+    global bool_shop
+    bool_shop = False
 
 #Class batiment
 class ShopBuilding:
@@ -297,19 +346,33 @@ level = Map(1024, 1024) #Créer une carte de 1024 * 1024
 drill = Player((level.width // 2, 0), level) #Créer le joueur ayant comme paramètre la carte
 hud = Interface(drill) #Créer une interface avec différentes barres (comme) le fuel) et compteurs (comme le gold)
 
+boutons = [
+    Button(textures["buy"], [103, 632], 20, speed_button, drill),
+    Button(textures["buy"], [391, 632], 20, fuel_button, drill),
+    Button(textures["buy"], [679, 632], 500, skin_button, drill),
+    Button(textures["exit"], [426, 691], 0, exit_button, drill),
+] #Tableau contenant chaque paramètres de chaque boutons
 shop = ShopBuilding((level.width // 2, 0), level)
 
+bool_shop = True #Faut changer ça, selon ce que tu veux (être dans le shop, ou sur la map princiaple)
 running = True
 while running: #Boucle principal qui execute toutes les fonctions à chaques frames
     clock.tick(60) 
 
-    level.render(drill.get_camera_offset()) #Affiche la carte avec une position de camera obtenue grace à fonction get_camera_offset()
-    shop.tick() #"Met a jour" la shop
-    drill.tick() #"Met a jour" la foreuse
-    hud.render() #"Met a jour" l'interface
+    if bool_shop:
+        for i in range(len(boutons)):
+            boutons[i].animation()
+    else:
+        level.render(drill.get_camera_offset()) #Affiche la carte avec une position de camera obtenue grace à fonction get_camera_offset()
+        shop.tick() #"Met a jour" la shop
+        drill.tick() #"Met a jour" la foreuse
+        hud.render() #"Met a jour" l'interface
 
     pygame.display.flip() #Met à jour l'affichage
 
     for event in pygame.event.get(): #Permet d'arrêter la boucle (Et donc le jeu si la fenêtre est fermée)
         if event.type == pygame.QUIT:
             running = False
+
+#Lignes par Lucas: 170
+#Lignes par Ugo: 80
